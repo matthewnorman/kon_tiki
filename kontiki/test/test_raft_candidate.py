5b984fdae23d3d31f9c7121f6dd7a5586a35bb1c
matthewnorman <mnorman.hep@gmail.com>
2014-12-20 22:29:30 -0800

    Fix the Raft Candidate test

--- kontiki/test/test_raft_candidate.py
+++ kontiki/test/test_raft_candidate.py
@@ -1,38 +1,56 @@
-from kontiki import raft
+from kontiki import raft, rpc
 from kontiki import persist
 from twisted.trial import unittest
 from twisted.internet import task
+from kontiki.test.common import dropResult
 
 
-class RaftCandidateTest:
+def applyCommand(*args):
+    return defer.succeed(None)
 
-    def create_candidate(self):
-        persister = persist.ListPersist()
+
+class RaftCandidateTest(unittest.TestCase):
+
+    def setUp(self):
+        persister = persist.SQLitePersist(':memory:')
+        persister.connect()
         identity = 'identity'
-        peers = set(['peer1', 'peer2', 'peer3', 'peer4', 'peer5'])
-        applyCommand = None
+        peers = set()
         timeoutRange = (.150, .350)
-        cycle = raft.ServerCycle(identity=identity, peers=peers,
-                                 applyCommand=applyCommand,
-                                 persister=persister)
+        self.server = rpc.RaftServer(identity=identity, peers=peers,
+                                     applyCommand=applyCommand,
+                                     persister=persister)
+        self.state = raft.Candidate(identity=identity,
+                                    server=self.server,
+                                    peers=peers,
+                                    applyCommand=applyCommand,
+                                    electionTimeoutRange=timeoutRange,
+                                    persister=persister)
+        
+        originalClock = raft.StartsElection.clock
+        self.patch(raft.StartsElection, 'clock', task.Clock())
 
-        def fake_conduct(*args):
-            pass
+        def restoreClock():
+            raft.StartsElection.clock = originalClock
 
-        candidate = raft.Candidate(identity=identity, cycle=cycle,
-                                   peers=peers, applyCommand=applyCommand,
-                                   electionTimeoutRange=timeoutRange,
-                                   persister=persister)
-        return candidate
+        self.addCleanup(restoreClock)
 
-    def test_prepareForElection(self):
-        """
-        Just make sure things are set up
+    def test_willBecomeLeader_False(self):
+        self.state.peers = set([1, 2])
+        results = self.state.willBecomeLeader(votesSoFar=0)
+        msg = 'Should be false with 0 votes'
+        results.addCallback(self.assertFalse, msg=msg)
+        results.addCallback(dropResult(self.assertTrue,
+                                       isinstance(self.server.state,
+                                                  raft.Follower)))
+        return results
 
-        """
-
-        candidate = self.create_candidate()
-        candidate.prepareForElection()
-        self.assertEqual(candidate.votes, 0)
-        self.assertEqual(candidate.persister.votedFor, 'identity')
-        self.assertEqual(candidate.persister.currentTerm, 1)
+    def test_willBecomeLeader_True(self):
+        self.state.peers = set([1, 2])
+        results = self.state.willBecomeLeader(votesSoFar=2)
+        msg = 'Should be true with 2 votes'
+        results.addCallback(self.assertFalse, msg=msg)
+        results.addCallback(dropResult(self.assertTrue,
+                                       isinstance(self.server.state,
+                                                  raft.Leader)))
+        return results
